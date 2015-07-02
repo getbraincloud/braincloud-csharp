494df16cc650c540a10ebadd08c11b19d3034f01
Brad Hill <bradleyh@bitheads.com>
2015-07-02 16:50:39 -0400

    Completed OneWayMatch test functions

--- BrainCloudClient/tests/TestOneWayMatch.cs
+++ BrainCloudClient/tests/TestOneWayMatch.cs
@@ -1,4 +1,3 @@
-using NUnit;
 using NUnit.Core;
 using NUnit.Framework;
 using BrainCloud;
@@ -9,16 +8,94 @@ namespace BrainCloudTests
 {
     [TestFixture]
     public class TestOneWayMatch : TestFixtureBase
-    {      
+    {
+        private readonly string _otherPlayerid = "029b1c4e-61ca-4a59-8475-9921349c9cc3";
+
         [Test]
-        public void Test()
+        public void TestStartMatch()
         {
             TestResult tr = new TestResult();
 
+            BrainCloudClient.Get().OneWayMatchService.StartMatch(
+                _otherPlayerid,
+                10,
+                tr.ApiSuccess, tr.ApiError);
+
+            tr.Run();
+
+            CancelMatch(GetStreamId());
+        }
+
+        [Test]
+        public void TestCancelMatch()
+        {
+            StartMatch();
+            string streamId = GetStreamId();
+            TestResult tr = new TestResult();
+
+            BrainCloudClient.Get().OneWayMatchService.CancelMatch(
+                streamId,
+                tr.ApiSuccess, tr.ApiError);
+
+            tr.Run();
+        }
+
+        [Test]
+        public void TestCompleteMatch()
+        {
+            StartMatch();
+            string streamId = GetStreamId();
+            TestResult tr = new TestResult();
+
+            BrainCloudClient.Get().OneWayMatchService.CompleteMatch(
+                streamId,
+                tr.ApiSuccess, tr.ApiError);
+
+            tr.Run();
+        }
+
+        #region Helper functions
+
+        private void StartMatch()
+        {
+            TestResult tr = new TestResult();
+
+            BrainCloudClient.Get().OneWayMatchService.StartMatch(
+                _otherPlayerid,
+                10,
+                tr.ApiSuccess, tr.ApiError);
+
+            tr.Run();
+        }
+
+        private void CancelMatch(string streamId)
+        {
+            TestResult tr = new TestResult();
+            BrainCloudClient.Get().OneWayMatchService.CancelMatch(
+                streamId,
+                tr.ApiSuccess, tr.ApiError);
+            tr.Run();
+        }
+
+        private string GetStreamId()
+        {
+            TestResult tr = new TestResult();
+
+            string streamId = "";
+
+            BrainCloudClient.Get().PlaybackStreamService.GetStreamSummariesForTargetPlayer(
+                _otherPlayerid,
+                tr.ApiSuccess, tr.ApiError);
+
             if (tr.Run())
             {
-                // something
+                Dictionary<string, object>[] streams = (Dictionary<string, object>[])(((Dictionary<string, object>)(tr.m_response["data"]))["streams"]);
+                streamId = (string)streams[0]["playbackStreamId"];
             }
+
+            return streamId;
         }
+
+        #endregion
     }
 }
