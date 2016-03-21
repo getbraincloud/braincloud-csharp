3d375ece2bb0a65900b7a3d2cb047b3a0a277509
Brad Hill <bradleyh@hitheads.com>
2016-03-21 12:03:51 -0400

    Fix ResetEmailPassword test

--- BrainCloudClient/tests/TestAuthenticate.cs
+++ BrainCloudClient/tests/TestAuthenticate.cs
@@ -53,17 +53,10 @@ namespace BrainCloudTests
         {
             TestResult tr = new TestResult();
 
-            BrainCloudClient.Instance.AuthenticationService.AuthenticateEmailPassword(
-                GetUser(Users.UserA).Email,
-                GetUser(Users.UserA).Password,
-                true,
-                tr.ApiSuccess, tr.ApiError);
-
-            tr.Run();
-            tr.Reset();
+            string email = "braincloudunittest@gmail.com";
 
             BrainCloudClient.Instance.AuthenticationService.ResetEmailPassword(
-                GetUser(Users.UserA).Email,
+                email,
                 tr.ApiSuccess, tr.ApiError);
 
             tr.Run();
