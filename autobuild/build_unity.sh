7cd7093266ed9361f21abf62bdf6bada44386575
bit-jonathanm <jonathanm@bitheads.com>
2018-01-12 18:18:22 -0500

    Adding Unity Auto build script

---
+++ autobuild/build_unity.sh
@@ -0,0 +1,40 @@
+cd /Users/jonm/Documents/Unity-Csharp/autobuild
+
+set -e
+
+build_version=1.2.3
+
+if [ "$build_version" == "" ]; then
+  echo "Must pass in build version"
+  exit 1
+fi
+
+rm -rf artifacts
+mkdir -p artifacts/brainCloudUnityPackage
+
+cur_path=$(pwd)
+
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+-batchmode \
+-nographics \
+-logFile /Users/jonm/Desktop/log-unity.txt \
+-projectPath $cur_path/../BrainCloudClient/ \
+-exportPackage \
+Assets/BrainCloud/Client/BrainCloud \
+Assets/BrainCloud/Client/BrainCloud/Common \
+Assets/BrainCloud/Client/BrainCloud/Entity \
+Assets/BrainCloud/Client/BrainCloud/Entity/Internal \
+Assets/BrainCloud/Client/BrainCloud/Internal \
+Assets/BrainCloud/JsonFx \
+Assets/BrainCloud/LitJson \
+Assets/BrainCloud/Resources \
+Assets/BrainCloud/Unity/BrainCloudUnityPlugin/Editor \
+Assets/BrainCloud/Unity/BrainCloudUnityPlugin/Scripts \
+Assets/BrainCloud/Unity/Editor \
+Assets/BrainCloud/Unity/Editor/Resources \
+Assets/BrainCloud/Unity/Editor/Resources/Images \
+Assets/BrainCloud/Unity/Prefabs \
+Assets/BrainCloud/Unity/Scenes \
+Assets/BrainCloud/Unity/Scripts \
+Assets/BrainCloud/Unity/Scripts/HUD \
+../autobuild/braincloudunity.unitypackage -quit
