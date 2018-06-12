6a397efa78fe36718cfdc3734e275552c4ff72c8
David St-Louis <davidstl@bitheads.com>
2018-06-12 08:46:45 -0400

    Made sure the new websocket code compiles on .net, but unimplemented (#34)

--- BrainCloudClient/Assets/BrainCloud/Client/BrainCloud/Internal/BrainCloudWebSocket.cs
+++ BrainCloudClient/Assets/BrainCloud/Client/BrainCloud/Internal/BrainCloudWebSocket.cs
@@ -1,25 +1,29 @@
 ﻿using System;
 
-#if UNITY_WEBGL && !UNITY_EDITOR
+#if DOT_NET
+using System.Net.WebSockets;
+#elif UNITY_WEBGL && !UNITY_EDITOR
 using AOT;
 using System.Collections.Generic;
 #else
 using WebSocketSharp;
 #endif
 
-public class UnityWebSocket
+public class BrainCloudWebSocket
 {
-#if UNITY_WEBGL && !UNITY_EDITOR
+#if DOT_NET
+#elif UNITY_WEBGL && !UNITY_EDITOR
 	private NativeWebSocket NativeWebSocket;   
-    private static Dictionary<int, UnityWebSocket> webSocketInstances =
-        new Dictionary<int, UnityWebSocket>();
+    private static Dictionary<int, BrainCloudWebSocket> webSocketInstances =
+        new Dictionary<int, BrainCloudWebSocket>();
 #else
     private WebSocket WebSocket;
 #endif
 
-    public UnityWebSocket(string url)
+    public BrainCloudWebSocket(string url)
     {
-#if UNITY_WEBGL && !UNITY_EDITOR
+#if DOT_NET
+#elif UNITY_WEBGL && !UNITY_EDITOR
 		NativeWebSocket = new NativeWebSocket(url);
 		NativeWebSocket.SetOnOpen(NativeSocket_OnOpen);
 		NativeWebSocket.SetOnMessage(NativeSocket_OnMessage);
@@ -39,7 +43,8 @@ public class UnityWebSocket
 
     public void Close()
     {
-#if UNITY_WEBGL && !UNITY_EDITOR
+#if DOT_NET
+#elif UNITY_WEBGL && !UNITY_EDITOR
         if (NativeWebSocket == null)
 			return;
 		NativeWebSocket.CloseAsync();
@@ -60,7 +65,8 @@ public class UnityWebSocket
 #endif
     }
 
-#if UNITY_WEBGL && !UNITY_EDITOR
+#if DOT_NET
+#elif UNITY_WEBGL && !UNITY_EDITOR
     [MonoPInvokeCallback(typeof(Action<int>))]
 	public static void NativeSocket_OnOpen(int id) {
 	
@@ -118,17 +124,18 @@ public class UnityWebSocket
 
     public void SendAsync(byte[] packet)
     {
-#if UNITY_WEBGL && !UNITY_EDITOR
+#if DOT_NET
+#elif UNITY_WEBGL && !UNITY_EDITOR
     	NativeWebSocket.SendAsync(packet);
 #else
         WebSocket.SendAsync(packet, null);
 #endif
     }
 
-    public delegate void OnOpenHandler(UnityWebSocket accepted);
-    public delegate void OnMessageHandler(UnityWebSocket sender, byte[] data);
-    public delegate void OnErrorHandler(UnityWebSocket sender, string message);
-    public delegate void OnCloseHandler(UnityWebSocket sender, int code, string reason);
+    public delegate void OnOpenHandler(BrainCloudWebSocket accepted);
+    public delegate void OnMessageHandler(BrainCloudWebSocket sender, byte[] data);
+    public delegate void OnErrorHandler(BrainCloudWebSocket sender, string message);
+    public delegate void OnCloseHandler(BrainCloudWebSocket sender, int code, string reason);
 
     public event OnOpenHandler OnOpen;
     public event OnMessageHandler OnMessage;
