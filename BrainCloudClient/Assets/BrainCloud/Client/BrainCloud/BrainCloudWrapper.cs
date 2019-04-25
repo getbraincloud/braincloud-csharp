2d7313996f9a12741235976268e0e07d46c823ac
Steve Jones <stevej@bitheads.com>
2019-04-25 15:10:17 -0400

    there we go

--- BrainCloudClient/Assets/BrainCloud/Client/BrainCloud/BrainCloudWrapper.cs
+++ BrainCloudClient/Assets/BrainCloud/Client/BrainCloud/BrainCloudWrapper.cs
@@ -350,12 +350,12 @@ public class BrainCloudWrapper
     public void Init()
     {
         Init(
-            BrainCloudSettings.Instance.DispatcherURL,
-            BrainCloudSettings.Instance.SecretKey,
-            BrainCloudSettings.Instance.AppId,
-            BrainCloudSettings.Instance.GameVersion);
+            BrainCloudSettingsManual.Instance.DispatcherURL,
+            BrainCloudSettingsManual.Instance.SecretKey,
+            BrainCloudSettingsManual.Instance.AppId,
+            BrainCloudSettingsManual.Instance.GameVersion);
 
-        Client.EnableLogging(BrainCloudSettings.Instance.EnableLogging);
+        Client.EnableLogging(BrainCloudSettingsManual.Instance.EnableLogging);
     }
 
     /// <summary>
@@ -365,12 +365,12 @@ public class BrainCloudWrapper
     public void InitWithApps()
     {
         InitWithApps(
-            BrainCloudSettings.Instance.DispatcherURL,
-            BrainCloudSettings.Instance.AppId,
-            BrainCloudSettings.Instance.AppIdSecrets,
-            BrainCloudSettings.Instance.GameVersion);
+            BrainCloudSettingsManual.Instance.DispatcherURL,
+            BrainCloudSettingsManual.Instance.AppId,
+            BrainCloudSettingsManual.Instance.AppIdSecrets,
+            BrainCloudSettingsManual.Instance.GameVersion);
 
-        Client.EnableLogging(BrainCloudSettings.Instance.EnableLogging);
+        Client.EnableLogging(BrainCloudSettingsManual.Instance.EnableLogging);
     }
 #endif
 
@@ -1499,7 +1499,7 @@ public class BrainCloudWrapper
         }
 
 #if UNITY_EDITOR
-        BrainCloudUnity.BrainCloudPlugin.ResponseEvent.OnAuthenticateSuccess(json);
+        BrainCloudUnity.BrainCloudSettingsDLL.ResponseEvent.OnAuthenticateSuccess(json);
 #endif
     }
 
@@ -1522,7 +1522,7 @@ public class BrainCloudWrapper
         }
 
 #if UNITY_EDITOR
-        BrainCloudUnity.BrainCloudPlugin.ResponseEvent.OnAuthenticateFailed(string.Format("statusCode[{0}] reasonCode[{1}] errorJson[{2}]", statusCode, reasonCode, errorJson));
+        BrainCloudUnity.BrainCloudSettingsDLL.ResponseEvent.OnAuthenticateFailed(string.Format("statusCode[{0}] reasonCode[{1}] errorJson[{2}]", statusCode, reasonCode, errorJson));
 #endif
     }
 
