5d14a9b90a9540de7be0615320e9bc90227552e9
ruth0048 <31827673+ruth0048@users.noreply.github.com>
2019-08-30 10:52:49 -0400

    Drop Packet Bugfix and Updated HTTP Libraries (#98)
    * adding ModernHTTP
    
    * fixing bug
    
    * cleanup
    
    * bad mistake fixed

---
+++ BrainCloudClient/Assets/BrainCloud/ModernHttpClient/TLSConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace BrainCloud
+{
+    namespace ModernHttpClient
+    {
+        public class TLSConfig
+        {
+            public TLSConfig() { }
+
+            public List<Pin> Pins { get; set; }
+            public ClientCertificate ClientCertificate { get; set; }
+            public bool DangerousAcceptAnyServerCertificateValidator { get; set; }
+            public bool DangerousAllowInsecureHTTPLoads { get; set; } // to match iOS NSAppTransportSecurity in Android
+        }
+
+        public class Pin
+        {
+            public Pin() { }
+
+            public string Hostname { get; set; }
+            public string[] PublicKeys { get; set; }
+        }
+
+        public class ClientCertificate
+        {
+            public ClientCertificate() { }
+
+            public string Passphrase { get; set; }
+            public string RawData { get; set; }
+        }
+    }
+}
