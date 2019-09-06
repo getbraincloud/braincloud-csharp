f91bc2baeb5f21a56fc9d83109a042b60b1a8987
Pascal <pascalf@bitheads.com>
2019-09-06 15:53:29 -0400

    finished service now starting writing unit tests

---
+++ BrainCloudClient/Assets/BrainCloud/Client/BrainCloud/BrainCloudCustomEntity.cs
@@ -0,0 +1,304 @@
+//----------------------------------------------------
+// brainCloud client source code
+// Copyright 2016 bitHeads, inc.
+//----------------------------------------------------
+
+namespace BrainCloud
+{
+
+using System.Collections.Generic;
+using BrainCloud.Internal;
+using BrainCloud.JsonFx.Json;
+using BrainCloud.Common;
+
+    public class BrainCloudItemCatalog
+    {
+        private BrainCloudClient _client;
+
+        public BrainCloudItemCatalog(BrainCloudClient client)
+        {
+            _client = client;
+        }
+
+        /// <summary>
+        /// Creates a Custom Entity
+        /// </summary>
+        /// <remarks>
+        /// Service Name - CustomEntity
+        /// Service Operation - CreateCustomEntity
+        /// </remarks>
+        /// <param name="entityType">
+        /// The Entity Type
+        /// </param>
+        /// <param name="dataJson">
+        /// The entity data
+        /// </param>
+        /// <param name="acl">
+        /// 
+        /// <param name="timeToLive">
+        /// The Entity Type
+        /// </param>
+        /// <param name="success">
+        /// The success callback.
+        /// </param>
+        /// <param name="failure">
+        /// The failure callback.
+        /// </param>
+        /// <param name="cbObject">
+        /// The user object sent to the callback.
+        /// </param>
+        public void CreateCustomEntity(
+        string entityType,
+        string dataJson,
+        string acl,
+        string timeToLive,
+        SuccessCallback success = null,
+        FailureCallback failure = null,
+        object cbObject = null)
+        {
+            Dictionary<string, object> data = new Dictionary<string, object>();
+            data[OperationParam.CustomEntityServiceEntityType.Value] = entityType;
+            data[OperationParam.CustomEntityServiceEntityDataJson.Value] = JsonReader.Deserialize<Dictionary<string, object>>(dataJson);
+            data[OperationParam.CustomEntityServiceEntityAcl.Value] = JsonReader.Deserialize<Dictionary<string, object>>(acl); 
+            data[OperationParam.CustomEntityServiceTimeToLive.Value] = timeToLive;
+
+            ServerCallback callback = BrainCloudClient.CreateServerCallback(success, failure, cbObject);
+            ServerCall sc = new ServerCall(ServiceName.ItemCatalog, ServiceOperation.GetCatalogItemDefinition, data, callback);
+            _client.SendRequest(sc);
+        }
+
+        /// <summary>
+        /// Retrieves page of custom entity from the server, 
+        ///with language fields limited to the text for the 
+        ///current or default language.
+        /// </summary>
+        /// <remarks>
+        /// Service Name - CustomEntity
+        /// Service Operation - GetCustomEntityPage
+        /// </remarks>
+        /// <param name="entityType">
+        /// </param>
+        /// <param name="rowsPerPage">
+        /// quantity of rows per page
+        /// </param>
+        /// <param name="searchJson">
+        /// 
+        /// </param>
+        /// <param name="sortJson">
+        /// 
+        /// </param>
+        /// <param name="doCount">
+        /// 
+        /// </param>
+        /// <param name="success">
+        /// The success callback.
+        /// </param>
+        /// <param name="failure">
+        /// The failure callback.
+        /// </param>
+        /// <param name="cbObject">
+        /// The user object sent to the callback.
+        /// </param>//this good
+        public void GetCustomEntityPage(
+        string entityType,
+        int rowsPerPage,
+        string searchJson,
+        string sortJson,
+        bool doCount,
+        SuccessCallback success = null,
+        FailureCallback failure = null,
+        object cbObject = null)
+        {
+            Dictionary<string, object> data = new Dictionary<string, object>();
+            data[OperationParam.CustomEntityServiceEntityType.Value] = entityType;
+            data[OperationParam.CustomEntityServiceEntityRowsPerPage.Value] = rowsPerPage;
+            data[OperationParam.CustomEntityServiceEntitySearchJson.Value] = JsonReader.Deserialize<Dictionary<string, object>>(searchJson);
+            data[OperationParam.CustomEntityServiceEntitySortJson.Value] = JsonReader.Deserialize<Dictionary<string, object>>(sortJson);
+            data[OperationParam.CustomEntityServiceEntityDoCount.Value] = doCount;
+
+            ServerCallback callback = BrainCloudClient.CreateServerCallback(success, failure, cbObject);
+            ServerCall sc = new ServerCall(ServiceName.ItemCatalog, ServiceOperation.GetCatalogItemsPage, data, callback);
+            _client.SendRequest(sc);
+        }
+
+        /// <summary>
+        /// Gets the page of custom entity from the 
+        ///server based on the encoded context and 
+        ///specified page offset, with language fields 
+        ///limited to the text for the current or default 
+        ///language.
+        /// </summary>
+        /// <remarks>
+        /// Service Name - CustomEntity
+        /// Service Operation - GetCustomEntityPageOffset
+        /// </remarks>
+        /// <param name="entityType">
+        /// </param>
+        /// <param name="context">
+        /// </param>
+        /// <param name="pageOffset">
+        /// </param>
+        /// <param name="success">
+        /// The success callback.
+        /// </param>
+        /// <param name="failure">
+        /// The failure callback.
+        /// </param>
+        /// <param name="cbObject">
+        /// The user object sent to the callback.
+        /// </param>this good
+        public void GetCustomEntityPageOffset(
+        string entityType,
+        string context,
+        int pageOffset,
+        SuccessCallback success = null,
+        FailureCallback failure = null,
+        object cbObject = null)
+        {
+            Dictionary<string, object> data = new Dictionary<string, object>();
+        // data[OperationParam.ItemCatalogServiceContext.Value] = context;
+        //data[OperationParam.ItemCatalogServicePageOffset.Value] = pageOffset;
+
+            data[OperationParam.CustomEntityServiceContext.Value] = context;
+            data[OperationParam.CustomEntityServicePageOffset.Value] = pageOffset;
+
+            ServerCallback callback = BrainCloudClient.CreateServerCallback(success, failure, cbObject);
+            ServerCall sc = new ServerCall(ServiceName.ItemCatalog, ServiceOperation.GetCatalogItemsPageOffset, data, callback);
+            _client.SendRequest(sc);
+        }
+
+        /// <summary>
+        /// Gets the page of catalog items from the 
+        ///server based on the encoded context and 
+        ///specified page offset, with language fields 
+        ///limited to the text for the current or default 
+        ///language.
+        /// </summary>
+        /// <remarks>
+        /// Service Name - ItemCatalog
+        /// Service Operation - GetCatalogItemDefinition
+        /// </remarks>
+        /// <param name="entityType">
+        /// </param>
+        /// <param name="entityId">
+        /// </param>
+        /// <param name="success">
+        /// The success callback.
+        /// </param>
+        /// <param name="failure">
+        /// The failure callback.
+        /// </param>
+        /// <param name="cbObject">
+        /// The user object sent to the callback.
+        /// </param>
+        public void ReadCustomEntity(
+        string entityType,
+        string entityId,
+        SuccessCallback success = null,
+        FailureCallback failure = null,
+        object cbObject = null)
+        {
+            Dictionary<string, object> data = new Dictionary<string, object>();
+            data[OperationParam.ItemCatalogServiceEntityType.Value] = entityType;
+            data[OperationParam.ItemCatalogServiceEntityId.Value] = entityId;
+
+            ServerCallback callback = BrainCloudClient.CreateServerCallback(success, failure, cbObject);
+            ServerCall sc = new ServerCall(ServiceName.ItemCatalog, ServiceOperation.GetCatalogItemsPageOffset, data, callback);
+            _client.SendRequest(sc);
+        }
+
+                /// <summary>
+        /// Gets the page of catalog items from the 
+        ///server based on the encoded context and 
+        ///specified page offset, with language fields 
+        ///limited to the text for the current or default 
+        ///language.
+        /// </summary>
+        /// <remarks>
+        /// Service Name - ItemCatalog
+        /// Service Operation - GetCatalogItemDefinition
+        /// </remarks>
+        /// <param name="entityType">
+        /// </param>
+        /// <param name="entityId">
+        /// </param>
+        /// <param name="version">
+        /// </param>
+        /// <param name="dataJson">
+        /// </param>
+        /// <param name="acl">
+        /// </param>
+        /// <param name="timeToLive">
+        /// </param>
+        /// <param name="success">
+        /// The success callback.
+        /// </param>
+        /// <param name="failure">
+        /// The failure callback.
+        /// </param>
+        /// <param name="cbObject">
+        /// The user object sent to the callback.
+        /// </param>
+        public void UpdateCustomEntity(
+        string entityType,
+        string entityId,
+        int version,
+        string dataJson,
+        string acl,
+        string timeToLive,
+        SuccessCallback success = null,
+        FailureCallback failure = null,
+        object cbObject = null)
+        {
+            Dictionary<string, object> data = new Dictionary<string, object>();
+            data[OperationParam.ItemCatalogServiceContext.Value] = context;
+            data[OperationParam.ItemCatalogServicePageOffset.Value] = pageOffset;
+
+            ServerCallback callback = BrainCloudClient.CreateServerCallback(success, failure, cbObject);
+            ServerCall sc = new ServerCall(ServiceName.ItemCatalog, ServiceOperation.GetCatalogItemsPageOffset, data, callback);
+            _client.SendRequest(sc);
+        }
+
+        
+
+        /// <summary>
+        /// Gets the page of catalog items from the 
+        ///server based on the encoded context and 
+        ///specified page offset, with language fields 
+        ///limited to the text for the current or default 
+        ///language.
+        /// </summary>
+        /// <remarks>
+        /// Service Name - ItemCatalog
+        /// Service Operation - GetCatalogItemDefinition
+        /// </remarks>
+        /// <param name="context">
+        /// </param>
+        /// <param name="pageOffset">
+        /// </param>
+        /// <param name="success">
+        /// The success callback.
+        /// </param>
+        /// <param name="failure">
+        /// The failure callback.
+        /// </param>
+        /// <param name="cbObject">
+        /// The user object sent to the callback.
+        /// </param>
+        public void UpdateCustomEntityFields(
+        string context,
+        int pageOffset,
+        SuccessCallback success = null,
+        FailureCallback failure = null,
+        object cbObject = null)
+        {
+            Dictionary<string, object> data = new Dictionary<string, object>();
+            data[OperationParam.ItemCatalogServiceContext.Value] = context;
+            data[OperationParam.ItemCatalogServicePageOffset.Value] = pageOffset;
+
+            ServerCallback callback = BrainCloudClient.CreateServerCallback(success, failure, cbObject);
+            ServerCall sc = new ServerCall(ServiceName.ItemCatalog, ServiceOperation.GetCatalogItemsPageOffset, data, callback);
+            _client.SendRequest(sc);
+        }
+    }
+}
---
+++ BrainCloudClient/tests/TestCustomEntityService.cs
@@ -0,0 +1,83 @@
+using NUnit.Core;
+using NUnit.Framework;
+using BrainCloud;
+using System.Collections.Generic;
+using BrainCloud.JsonFx.Json;
+using BrainCloud.Common;
+
+namespace BrainCloudTests
+{
+    [TestFixture]
+    public class TestCustomEntityService : TestFixtureBase
+    {
+        [Test]
+        public void TestCreateCustomEntity()
+        {
+            TestResult tr = new TestResult(_bc);
+            _bc.CustomEntityService.CreateCustomEntity(
+                "sword001",
+                tr.ApiSuccess, tr.ApiError);
+            tr.Run();
+        }
+
+        [Test]
+        public void TestGetCustomEntityPage()
+        {
+            string context = "{\"test\": \"Testing\"}";
+            TestResult tr = new TestResult(_bc);
+
+            _bc.ItemCatalogService.GetCustomEntityPage(
+                context,
+                tr.ApiSuccess, tr.ApiError);
+            tr.Run();
+        }
+
+        [Test]
+        public void TestGetCustomEntityPageOffset()
+        {
+            string context = "eyJzZWFyY2hDcml0ZXJpYSI6eyJjYXRlZ29yeSI6InN3b3JkIiwiZ2FtZUlkIjoiMjAwMDEifSwic29ydENyaXRlcmlhIjp7ImNyZWF0ZWRBdCI6MSwidXBkYXRlZEF0IjotMX0sInBhZ2luYXRpb24iOnsicm93c1BlclBhZ2UiOjUwLCJwYWdlTnVtYmVyIjoxfSwib3B0aW9ucyI6bnVsbH0";
+            TestResult tr = new TestResult(_bc);
+            _bc.ItemCatalogService.GetCustomEntityPageOffset(
+                context,
+                1,
+                tr.ApiSuccess, tr.ApiError);
+            tr.Run();
+        }
+
+        [Test]
+        public void ReadCustomEntity()
+        {
+            string context = "eyJzZWFyY2hDcml0ZXJpYSI6eyJjYXRlZ29yeSI6InN3b3JkIiwiZ2FtZUlkIjoiMjAwMDEifSwic29ydENyaXRlcmlhIjp7ImNyZWF0ZWRBdCI6MSwidXBkYXRlZEF0IjotMX0sInBhZ2luYXRpb24iOnsicm93c1BlclBhZ2UiOjUwLCJwYWdlTnVtYmVyIjoxfSwib3B0aW9ucyI6bnVsbH0";
+            TestResult tr = new TestResult(_bc);
+            _bc.ItemCatalogService.ReadCustomEntity(
+                context,
+                1,
+                tr.ApiSuccess, tr.ApiError);
+            tr.Run();
+        }
+
+        [Test]
+        public void TestUpdateCustomEntity()
+        {
+            string context = "eyJzZWFyY2hDcml0ZXJpYSI6eyJjYXRlZ29yeSI6InN3b3JkIiwiZ2FtZUlkIjoiMjAwMDEifSwic29ydENyaXRlcmlhIjp7ImNyZWF0ZWRBdCI6MSwidXBkYXRlZEF0IjotMX0sInBhZ2luYXRpb24iOnsicm93c1BlclBhZ2UiOjUwLCJwYWdlTnVtYmVyIjoxfSwib3B0aW9ucyI6bnVsbH0";
+            TestResult tr = new TestResult(_bc);
+            _bc.ItemCatalogService.TestCustomEntity(
+                context,
+                1,
+                tr.ApiSuccess, tr.ApiError);
+            tr.Run();
+        }
+
+        [Test]
+        public void TestUpdateCustomEntityFields()
+        {
+            string context = "eyJzZWFyY2hDcml0ZXJpYSI6eyJjYXRlZ29yeSI6InN3b3JkIiwiZ2FtZUlkIjoiMjAwMDEifSwic29ydENyaXRlcmlhIjp7ImNyZWF0ZWRBdCI6MSwidXBkYXRlZEF0IjotMX0sInBhZ2luYXRpb24iOnsicm93c1BlclBhZ2UiOjUwLCJwYWdlTnVtYmVyIjoxfSwib3B0aW9ucyI6bnVsbH0";
+            TestResult tr = new TestResult(_bc);
+            _bc.ItemCatalogService.UpdateCustomEntityFields(
+                context,
+                1,
+                tr.ApiSuccess, tr.ApiError);
+            tr.Run();
+        }
+    }
+}
