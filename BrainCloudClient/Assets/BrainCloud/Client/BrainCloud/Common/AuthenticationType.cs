9da16feecebfbe24d2c036bb18dd771248b72917
ruth0048 <31827673+ruth0048@users.noreply.github.com>
2020-01-17 15:30:14 -0500

    authenitcate google open Id (#122)
    * AuthenticateGoogleOpenId and edits to params of authenicateGoogle
    
    * fixing auth type
    
    * adding parameter for ne auth type

--- BrainCloudClient/Assets/BrainCloud/Client/BrainCloud/Common/AuthenticationType.cs
+++ BrainCloudClient/Assets/BrainCloud/Client/BrainCloud/Common/AuthenticationType.cs
@@ -18,6 +18,7 @@ namespace BrainCloud.Common
         public static readonly AuthenticationType GameCenter = new AuthenticationType("GameCenter");
         public static readonly AuthenticationType Steam = new AuthenticationType("Steam");
         public static readonly AuthenticationType Google = new AuthenticationType("Google");
+        public static readonly AuthenticationType GoogleOpenId = new AuthenticationType("GoogleOpenId");
         public static readonly AuthenticationType Twitter = new AuthenticationType("Twitter");
         public static readonly AuthenticationType Parse = new AuthenticationType("Parse");
         public static readonly AuthenticationType External = new AuthenticationType("External");
@@ -34,6 +35,7 @@ namespace BrainCloud.Common
             { GameCenter.value, GameCenter },
             { Steam.value, Steam },
             { Google.value, Google },
+            { GoogleOpenId.value, GoogleOpenId },
             { Twitter.value, Twitter },
             { Parse.value, Parse },
             { Handoff.value, Handoff},
