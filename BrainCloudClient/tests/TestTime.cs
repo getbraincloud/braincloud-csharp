e18d845746bbb2c492f374203898613ba4090608
ruth0048 <31827673+ruth0048@users.noreply.github.com>
2020-04-28 22:06:21 -0400

    Time Util fixed and added

--- BrainCloudClient/tests/TestTime.cs
+++ BrainCloudClient/tests/TestTime.cs
@@ -1,6 +1,7 @@
 using NUnit.Core;
 using NUnit.Framework;
 using BrainCloud;
+using System;
 
 namespace BrainCloudTests
 {
@@ -16,6 +17,39 @@ namespace BrainCloudTests
                 tr.ApiSuccess, tr.ApiError);
 
             tr.Run();
+        }
+
+        [Test]
+        public void TestTimeUtil()
+        {
+            DateTime _dateBefore = TimeUtil.LocalTimeToUTCTime(DateTime.Now); 
+            Console.WriteLine("Date: " + _dateBefore);
+            Int64 _dateTimeMillis = TimeUtil.UTCDateTimeToUTCMillis(_dateBefore);
+            Console.WriteLine("Date Converted to Millis: " + _dateTimeMillis);
+            DateTime _dateAfter = TimeUtil.UTCMillisToUTCDateTime(_dateTimeMillis);
+            Console.WriteLine("Date Converted back: " + _dateAfter);
+            Int64 _dateAfterMillis = TimeUtil.UTCDateTimeToUTCMillis(_dateAfter);
+            Console.WriteLine("Date Converted to Millis: " + _dateAfterMillis);
+            //in c# the z time in UTC can differ by up to 0.9 seconds when converted. So the assert will never match
+            Assert.AreEqual(_dateTimeMillis, _dateAfterMillis);
+
+            DateTime _localDate = DateTime.Now; 
+            Console.WriteLine("Local Date: " + _localDate);
+            DateTime _utcDate = TimeUtil.LocalTimeToUTCTime(_localDate);
+            Console.WriteLine("UTC Date: " + _utcDate);
+            DateTime _newDate = TimeUtil.UTCTimeToLocalTime(_utcDate);
+            Console.WriteLine("Converted back to Local: " + _newDate);
+            Assert.AreEqual(_localDate, _newDate);
+
+            DateTimeOffset _dateOffset = TimeUtil.LocalTimeToUTCTime(DateTimeOffset.Now);
+            Console.WriteLine("DateTime Offset: " + _dateOffset);
+            Int64 _ms = TimeUtil.DateTimeOffsetToUTCMillis(_dateOffset);
+            Console.WriteLine("UTC Millis of offset: " + _ms);
+            DateTimeOffset _dateOffsetAfter = TimeUtil.UTCMillisToDateTimeOffset(_ms);
+            Console.WriteLine("DateTime Offset After: " + _dateOffsetAfter);
+            Int64 _msAfter = TimeUtil.DateTimeOffsetToUTCMillis(_dateOffsetAfter);
+            Console.WriteLine("UTC Millis of offset After: " + _msAfter);
+            Assert.AreEqual(_ms, _msAfter);
         }
     }
 }
