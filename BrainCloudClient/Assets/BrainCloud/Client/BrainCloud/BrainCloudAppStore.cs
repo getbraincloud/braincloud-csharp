969f3ac730793017507ce3c3873dc37465a7c64a
francobithead <82105876+francobithead@users.noreply.github.com>
2021-05-31 08:35:56 -0400

    Implementing Refresh Promotions (#191)
    * Implementing Refresh Promotions
    
    * Pull Request Changes from David

--- BrainCloudClient/Assets/BrainCloud/Client/BrainCloud/BrainCloudAppStore.cs
+++ BrainCloudClient/Assets/BrainCloud/Client/BrainCloud/BrainCloudAppStore.cs
@@ -294,5 +294,31 @@ using System;
             ServerCall sc = new ServerCall(ServiceName.AppStore, ServiceOperation.FinalizePurchase, data, callback);
             _client.SendRequest(sc);
         }
+        
+        /// <summary>
+        /// Returns up-to-date eligible 'promotions' for the user and a 'promotionsRefreshed' flag indicating whether the user's promotion info required refreshing
+        /// Service Name - appStore
+        /// Service Operation - RefreshPromotions
+        /// /// <param name="success">
+        /// The success callback.
+        /// </param>
+        /// <param name="failure">
+        /// The failure callback.
+        /// </param>
+        /// <param name="cbObject">
+        /// The user object sent to the callback.
+        /// </param>
+        /// </summary>
+        public void RefreshPromotions(
+            SuccessCallback success = null,
+            FailureCallback failure = null,
+            object cbObject = null)
+        {
+            Dictionary<string, object> data = new Dictionary<string, object>();
+            
+            ServerCallback callback = new ServerCallback(success, failure, cbObject);
+            ServerCall sc = new ServerCall(ServiceName.AppStore, ServiceOperation.RefreshPromotions, data, callback);
+            _client.SendRequest(sc);
+        }
     }
 }
--- BrainCloudClient/tests/TestAppStore.cs
+++ BrainCloudClient/tests/TestAppStore.cs
@@ -63,5 +63,14 @@ namespace BrainCloudTests
             _bc.AppStoreService.FinalizePurchase("_invalid_store_id_", "_invalid_transaction_id_", "{}", tr.ApiSuccess, tr.ApiError);
             tr.RunExpectFail(StatusCodes.BAD_REQUEST, ReasonCodes.INVALID_STORE_ID);
         }
+
+        [Test]
+        public void TestRefreshPromotions()
+        {
+            TestResult tr = new TestResult(_bc);
+
+            _bc.AppStoreService.RefreshPromotions(tr.ApiSuccess, tr.ApiError);
+            tr.Run();
+        }
     }
 }
