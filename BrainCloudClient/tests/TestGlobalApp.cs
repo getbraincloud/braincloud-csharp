84305861a7569d9db76e90bbf6dada8f79e7e63b
francobithead <82105876+francobithead@users.noreply.github.com>
2021-09-28 09:26:18 -0400

    New API calls ReadSelectedProperties & ReadPropertiesInCategories (#213)
    * New API calls ReadSelectedProperties & ReadPropertiesInCategories

--- BrainCloudClient/tests/TestGlobalApp.cs
+++ BrainCloudClient/tests/TestGlobalApp.cs
@@ -8,12 +8,39 @@ namespace BrainCloudTests
     public class TestGlobalApp : TestFixtureBase
     {
         [Test]
-        public void TestBrainCloudGlobalApp()
+        public void TestGlobalAppReadProperties()
         {
             TestResult tr = new TestResult(_bc);
 
             _bc.GlobalAppService.ReadProperties(
                 tr.ApiSuccess, tr.ApiError);
+
+            tr.Run();
+        }
+        
+        [Test]
+        public void TestGlobalAppReadSelectedProperties()
+        {
+            TestResult tr = new TestResult(_bc);
+            string[] propertyIds =
+            {
+                "prop1"
+            };
+
+            _bc.GlobalAppService.ReadSelectedProperties(propertyIds,tr.ApiSuccess, tr.ApiError);
+
+            tr.Run();
+        }
+        
+        [Test]
+        public void TestGlobalAppReadPropertiesInCategories()
+        {
+            TestResult tr = new TestResult(_bc);
+            string[] categories =
+            {
+                "Test"
+            };
+            _bc.GlobalAppService.ReadPropertiesInCategories(categories, tr.ApiSuccess, tr.ApiError);
 
             tr.Run();
         }
