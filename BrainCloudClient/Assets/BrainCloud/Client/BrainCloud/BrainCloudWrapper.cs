81c254bac48463e38a0a9b08a6a1c42e6ff3f5cd
franco <82105876+francobithead@users.noreply.github.com>
2022-02-10 13:04:19 -0500

    Added summary for Smart Switch Authenticate Ultra

--- BrainCloudClient/Assets/BrainCloud/Client/BrainCloud/BrainCloudWrapper.cs
+++ BrainCloudClient/Assets/BrainCloud/Client/BrainCloud/BrainCloudWrapper.cs
@@ -1921,7 +1921,12 @@ public class BrainCloudWrapper
     }
     
     /// <summary>
+    /// Smart Switch Authenticate will logout of the current profile, and switch to the new authentication type.
+    /// In event the current session was previously an anonymous account, the smart switch will delete that profile.
+    /// Use this function to keep a clean designflow from anonymous to signed profiles
+    /// 
     /// Authenticate the user for Ultra.
+    /// 
     /// </summary>
     /// <remarks>
     /// Service Name - Authenticate
