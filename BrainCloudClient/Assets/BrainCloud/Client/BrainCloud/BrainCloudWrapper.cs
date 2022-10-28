6d70750e2fbc9f49af84d1bab4ee1c3e714c59b5
franco <82105876+francobithead@users.noreply.github.com>
2022-10-28 10:34:58 -0400

    Playstation 5 Authenticate + Identity Calls
    - AuthenticatePlaystation5() for BrainCloudAuthenticate and Wrapper
    - Attach, Detach and Merge functions for Playstation5 for Identity calls.

--- BrainCloudClient/Assets/BrainCloud/Client/BrainCloud/BrainCloudWrapper.cs
+++ BrainCloudClient/Assets/BrainCloud/Client/BrainCloud/BrainCloudWrapper.cs
@@ -824,6 +824,45 @@ public class BrainCloudWrapper
     }
 
     /// <summary>
+    /// Authenticate the user using their psn account id and an auth token
+    /// </summary>
+    /// <remarks>
+    /// Service Name - Authenticate
+    /// Service Operation - Authenticate
+    /// </remarks>
+    /// <param name="accountId">
+    /// The user's PSN account id
+    /// </param>
+    /// <param name="authToken">
+    /// The user's PSN auth token
+    /// </param>
+    /// <param name="forceCreate">
+    /// Should a new profile be created for this user if the account does not exist?
+    /// </param>
+    /// <param name="success">
+    /// The method to call in event of successful login
+    /// </param>
+    /// <param name="failure">
+    /// The method to call in the event of an error during authentication
+    /// </param>
+    /// <param name="cbObject">
+    /// The user supplied callback object
+    /// </param>
+    public void AuthenticatePlaystation5(
+        string accountId,
+        string authToken,
+        bool forceCreate,
+        SuccessCallback success = null,
+        FailureCallback failure = null,
+        object cbObject = null)
+    {
+        WrapperAuthCallbackObject aco = MakeWrapperAuthCallback(success, failure, cbObject);
+
+        Client.AuthenticationService.AuthenticatePlaystation5(
+            accountId, authToken, forceCreate, AuthSuccessCallback, AuthFailureCallback, aco);
+    }
+    
+    /// <summary>
     /// Authenticate the user using their Game Center id
     /// </summary>
     /// <remarks>
