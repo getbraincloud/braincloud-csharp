b24661da375b0670c8d58c81880ed394a1016cc4
franco <82105876+francobithead@users.noreply.github.com>
2023-05-30 16:01:49 -0400

    Taking out Logout from Shutdown() to be consistent with other languages

--- BrainCloudClient/Assets/BrainCloud/Client/BrainCloud/Internal/BrainCloudComms.cs
+++ BrainCloudClient/Assets/BrainCloud/Client/BrainCloud/Internal/BrainCloudComms.cs
@@ -757,11 +757,7 @@ using UnityEngine.Experimental.Networking;
             {
                 _serviceCallsWaiting.Clear();
             }
-
-            // force a log out
-            ServerCallback callback = BrainCloudClient.CreateServerCallback(null, null, null);
-            ServerCall sc = new ServerCall(ServiceName.PlayerState, ServiceOperation.Logout, null, callback);
-            AddToQueue(sc);
+            
             DisposeUploadHandler();
             _activeRequest = null;
 
