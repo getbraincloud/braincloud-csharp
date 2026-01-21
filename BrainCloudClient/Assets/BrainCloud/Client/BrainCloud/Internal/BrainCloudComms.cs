7c617d9eeab185b4f49769fcfe0f16b74f1242ce
Franco Lago <82105876+francobithead@users.noreply.github.com>
2026-01-21 15:31:01 -0500

    Patch Release for 5.9.2 (#356)
    * Fix twice decompressing gziped responses in DOTNET client on iOS devices
    
    It seems that stream automatically decompressing by underlaying native implementation of HttpHandler or HttpClient on iOS
    
    * Updating ClientSideCompressionThreshold variable from 50000 to 51200 (#355)

--- BrainCloudClient/Assets/BrainCloud/Client/BrainCloud/Internal/BrainCloudComms.cs
+++ BrainCloudClient/Assets/BrainCloud/Client/BrainCloud/Internal/BrainCloudComms.cs
@@ -72,7 +72,7 @@ using UnityEngine.Experimental.Networking;
         /// Byte size threshold that determines if the message size is something we want to compress or not. We make an initial value, but recevie the value for future calls based on the servers 
         ///auth response
         /// </summary>
-        public int ClientSideCompressionThreshold{get; private set;} = 50000;
+        public int ClientSideCompressionThreshold{get; private set;} = 51200;
 
         /// <summary>
         /// The id of _expectedIncomingPacketId when no packet expected
@@ -1870,6 +1870,10 @@ using UnityEngine.Experimental.Networking;
                     req.Content.Headers.Add("Content-Encoding", "gzip");
                 }
 
+                req.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json") {
+                    CharSet = Encoding.UTF8.WebName
+                };
+
                 req.Headers.Add("X-SIG", sig);
                 if (AppId != null && AppId.Length > 0) 
                 {
@@ -1921,6 +1925,12 @@ using UnityEngine.Experimental.Networking;
                 outputStream.Read(compressedBytes, 0, compressedBytes.Length);
                 return outputStream.ToArray();
             }
+        }
+
+        private bool IsGzip(byte[] data)
+        {
+            if (data == null || data.Length < 2) return false;
+            return data[0] == 0x1F && data[1] == 0x8B;
         }
 
         /// <summary>
@@ -2242,7 +2252,7 @@ using UnityEngine.Experimental.Networking;
                 else
                 {
                     var byteArray = await content.ReadAsByteArrayAsync();
-                    var decompressedByteArray = Decompress(byteArray);
+                    var decompressedByteArray = IsGzip(byteArray) ? Decompress(byteArray) : byteArray;
                     requestState.DotNetResponseString = Encoding.UTF8.GetString(decompressedByteArray, 0, decompressedByteArray.Length);
                 }
                 
